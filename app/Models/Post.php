5ceac3754896dcfe699a0b0b2ab4ed05664ed1ae
programer10 <programer10@ucione.local>
2018-03-21 21:44:04 +0100

    Partially implement post feature, add slug plugin
    slug: cviebrock/eloquent-sluggable 4.3 (for laravel 5.5)

---
+++ app/Models/Post.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+
+class Post extends Model
+{
+    use Sluggable;
+
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+            'title',
+            'content',
+            'user_id'
+        ];
+
+    /**
+     * Save new post
+     *
+     * @param array $post
+     * @return object Post
+     */
+    public function savePost($post) : object {
+        return $this->create($post);
+    }
+
+    /**
+     * Update post
+     *
+     * @param array $post
+     * @return void
+     */
+    public function updatePost($post) : void {
+        $this->update($post);
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
