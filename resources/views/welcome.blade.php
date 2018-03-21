6a90b74acd6b7b0ece6d0a46be155785aaaad485
namagi <br.mladen@gmail.com>
2018-03-21 10:33:26 +0100

    Change header title and page title/branding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>
+            Algebra blog - {{ (strstr($_SERVER['SERVER_SOFTWARE'], 'Development')) ? 'Dev server' : 'Apache' }}
+        </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -50,17 +52,25 @@
             }
 
             .links > a {
-                color: #636b6f;
+                font-family: 'Raleway-SemiBold', sans-serif;
+                color: #000;
                 padding: 0 25px;
                 font-size: 12px;
-                font-weight: 600;
+                /*font-weight: 600;*/
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
             }
 
+            .links a:hover {
+                color: #838b8f;
+            }
             .m-b-md {
                 margin-bottom: 30px;
+            }
+
+            p.strong {
+                font-weight: 900;
             }
         </style>
     </head>
@@ -79,7 +89,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <p class="strong">Algebra demo blog</p>
                 </div>
 
                 <div class="links">
